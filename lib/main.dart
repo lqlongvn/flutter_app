ff180fbc08855567026d4c447e331e8efbf7c78f
lqlongvn <lqlongvn@gmail.com>
2021-09-03 21:05:33 +0700

    Button Code(3)(3/9/21 21h03): Như code(2), nhưng dùng Function Builder

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,21 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Scaffold đang là cha của MyButtonWidget
+    return Scaffold(
       body: Center(
-        child: MyButtonWidget(),
+        child: Builder( // widget Builder wrap widget FlatButton
+          // sử dụng thuộc tính builder
+          // truyền vào 1 biến hàm có type: Widget Function(BuildContext context)
+          builder: (context) => FlatButton(
+            child: Text('show snackbar'),
+            color: Colors.pink,
+            onPressed: () {
+              final snackBar = SnackBar(content: Text('Lỗi không thể truy cập bài viết này vì thấy hay mà không vote'));
+              Scaffold.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ),
       ),
-    );
-  }
-}
-class MyButtonWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) { // từ context của MyButtonWidget sẽ tìm được widget Scaffold cha gần nhất
-    return FlatButton(
-      child: Text('show snackbar'),
-      color: Colors.pink,
-      onPressed: () {
-        final snackBar = SnackBar(content: Text('Lỗi không thể truy cập bài viết này vì thấy hay mà không vote'));
-        Scaffold.of(context).showSnackBar(snackBar); // truyền vào context của MyButtonWidget
-      },
     );
   }
 }
