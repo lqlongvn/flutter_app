6f13e4f09350e8ce64129ef07722e7a6bf96de1e
lqlongvn <lqlongvn@gmail.com>
2021-12-16 10:58:28 +0700

    Button Code(3)(16/12/21 11h):  example từ Book Quick Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,10 @@
-import 'package:flutter/material.dart';
 
 main(){
-  print("Hi Long, Hello World");
-  int myNumber;
-
-  print(myNumber);
+  String saySomething = "Hello Long";
+  var isFalse = true;
+  if (saySomething == null){
+    print("It is ${isFalse}");
+  }else print("It is not ${isFalse}");
 }
 
-
-// void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Builder( // widget Builder wrap widget FlatButton
-          // sử dụng thuộc tính builder
-          // truyền vào 1 biến hàm có type: Widget Function(BuildContext context)
-          builder: (context) => FlatButton(
-            child: Text('show snackbar'),
-            color: Colors.pink,
-            onPressed: () {
-              final snackBar = SnackBar(content: Text('Lỗi không thể truy cập bài viết này vì thấy hay mà không vote'));
-              Scaffold.of(context).showSnackBar(snackBar);
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
 
