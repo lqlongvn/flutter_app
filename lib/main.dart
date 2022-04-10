8e6463e151784b563bd06268ba6d4c7888ea58ce
lqlongvn <lqlongvn@gmail.com>
2022-04-10 15:18:58 +0700

    Practical; The basics, tr83

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 
 import 'package:flutter/material.dart';
-
-main(){
-  var dog = new Dog();
-  dog.talk();
-}
-
-class Dog{
-  int legs;
-  Dog(){
-    print("default constructor");
-  }
-  void talk() {
-    print("Gau gau");
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title : "Flutter Playground",
+        home : Scaffold(
+            body : Center(
+                child : Row(
+                    children : [
+                      Text("Child1"),
+                      Text("Child2"),
+                      Text("Child3")
+                    ]
+                )
+            )
+        )
+    );
   }
 }
 
