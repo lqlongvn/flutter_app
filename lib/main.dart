f412a4a4000bc40aec7510d76d18fcdf347e7f97
lqlongvn <lqlongvn@gmail.com>
2022-04-11 08:33:31 +0700

    Chuyển trang; The BottomNavigationBar widget; tr100

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,47 @@
 
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key : key);
+  @override
+  _MyApp createState() => _MyApp();
+}
+class _MyApp extends State {
+  var _currentPage = 0;
+  var _pages = [
+    Text("Page 1 - Announcements"),
+    Text("Page 2 - Birthdays"),
+    Text("Page 3 - Data")
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(title : "Flutter Playground",
         home : Scaffold(
-            appBar : AppBar(
-                title : Text("Flutter Playground!")
-            ),
-            drawer : Drawer(
-                child : Column(
-                    children : [
-                      Text("Item 1"),
-                      Divider(),
-                      Text("Item 2"),
-                      Divider(),
-                      Text("Item 3")
-                    ]
-                )
-            ),
-
-            body : Center(
-                child : Row(
-                    children : [
-                      Text("Child1"),
-                      Text("Child2"),
-                      Text("Child3")
-                    ]
-                )
+            body : Center(child : _pages.elementAt(_currentPage)),
+            bottomNavigationBar : BottomNavigationBar(
+                items : [
+                  BottomNavigationBarItem(
+                      icon : Icon(Icons.announcement),
+                      title : Text("Announcements")
+                  ),
+                  BottomNavigationBarItem(
+                      icon : Icon(Icons.cake),
+                      title : Text("Birthdays")
+                  ),
+                  BottomNavigationBarItem(
+                      icon : Icon(Icons.cloud),
+                      title : Text("Data")
+                  ),
+                ],
+                currentIndex : _currentPage,
+                fixedColor : Colors.red,
+                onTap : (int inIndex) {
+                  setState(() { _currentPage = inIndex; });
+                }
             )
         )
     );
   }
 }
-
-
 
 
