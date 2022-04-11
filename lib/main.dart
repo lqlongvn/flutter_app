f22cc080ab97075abc0931900ae5aa2e18189a11
lqlongvn <lqlongvn@gmail.com>
2022-04-11 11:09:43 +0700

    TabBar Widget; Tr104;

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,32 @@
 
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key : key);
-  @override
-  _MyApp createState() => _MyApp();
-}
-class _MyApp extends State {
-  var _currentPage = 0;
-  var _pages = [
-    Text("Page 1 - Announcements"),
-    Text("Page 2 - Birthdays"),
-    Text("Page 3 - Data")
-  ];
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title : "Flutter Playground",
-        home : Scaffold(
-            body : Center(child : _pages.elementAt(_currentPage)),
-            bottomNavigationBar : BottomNavigationBar(
-                items : [
-                  BottomNavigationBarItem(
-                      icon : Icon(Icons.announcement),
-                      title : Text("Announcements")
-                  ),
-                  BottomNavigationBarItem(
-                      icon : Icon(Icons.cake),
-                      title : Text("Birthdays")
-                  ),
-                  BottomNavigationBarItem(
-                      icon : Icon(Icons.cloud),
-                      title : Text("Data")
-                  ),
-                ],
-                currentIndex : _currentPage,
-                fixedColor : Colors.red,
-                onTap : (int inIndex) {
-                  setState(() { _currentPage = inIndex; });
-                }
+    return MaterialApp(
+        home : DefaultTabController(
+            length : 3,
+            child : Scaffold(
+                appBar : AppBar(title : Text("Flutter Playground"),
+                    bottom : TabBar(
+                        tabs : [
+                          Tab(icon : Icon(Icons.announcement)),
+                          Tab(icon : Icon(Icons.cake)),
+                          Tab(icon : Icon(Icons.cloud))
+                        ]
+                    )
+                ),
+                body : TabBarView(
+                    children : [
+                      Center(child : Text("Announcements")),
+                      Center(child : Text("Birthdays")),
+                      Center(child : Text("Data"))
+                    ]
+                )
             )
         )
     );
   }
 }
-
 
