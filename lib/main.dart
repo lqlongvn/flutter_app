6bb9de0001d5c04f101c1e00a46dd970a6686204
lqlongvn <lqlongvn@gmail.com>
2022-05-04 21:20:24 +0700

    Table; tr149

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,41 @@
-import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home : Scaffold(body : Home()));
-  }
-}
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext inContext) {
-    Future _showIt() {
-      showModalBottomSheet(context : inContext,
-          builder : (BuildContext inContext) {
-            return new Column(
-                mainAxisSize : MainAxisSize.min,
-                children : [
-                  Text("What's your favorite pet?"),
-                  FlatButton(child : Text("Dog"),
-                    onPressed : () { Navigator.of(inContext).pop(); },
-                  ),
-                  FlatButton(child : Text("Cat"),
-                    onPressed : () { Navigator.of(inContext).pop(); },
-                  ),
-                  FlatButton(child : Text("Ferret"),
-                      onPressed : () { Navigator.of(inContext).pop(); }
-                  )
-                ]
-            );
-          }
-      );
-    }
 
-    return Scaffold(
-        body : Center(
-            child : RaisedButton(
-                child : Text("Show it"),
-                onPressed : _showIt
-            )
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext inContext) {
+    return MaterialApp(home : Scaffold(
+        body : Column(children : [
+            Container(height : 100),
+        Table(
+        border : TableBorder(
+        top : BorderSide(width : 2),
+        bottom : BorderSide(width : 2),
+        left : BorderSide(width : 2),
+        right : BorderSide(width : 2)
+    ),
+    children : [
+    TableRow(
+    children : [
+    Center(child : Padding(
+    padding : EdgeInsets.all(10),
+    child : Text("1"))
+    ),
+    Center(child : Padding(
+    padding : EdgeInsets.all(10),
+    child : Text("2"))
+    ),
+    Center(child : Padding(
+    padding : EdgeInsets.all(10),
+    child : Text("3"))
+    )
+    ]
+    )
+    ]
         )
-    );
+        ])
+    ));
   }
 }
+
