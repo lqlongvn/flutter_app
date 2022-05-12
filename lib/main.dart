c8cda243e2f4a4073255644cb3cc1119f26d7ebf
lqlongvn <lqlongvn@gmail.com>
2022-05-12 21:46:48 +0700

    ListView-5

--- lib/main.dart
+++ lib/main.dart
@@ -28,24 +28,23 @@ class BodyLayout extends StatelessWidget {
 
 // replace this function with the code in the examples
 Widget _myListView(BuildContext context) {
-  // backing data
-  final europeanCountries = ['Albania', 'Andorra', 'Armenia', 'Austria',
-    'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria',
-    'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland',
-    'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland',
-    'Italy', 'Kazakhstan', 'Kosovo', 'Latvia', 'Liechtenstein', 'Lithuania',
-    'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro',
-    'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia',
-    'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden',
-    'Switzerland', 'Turkey', 'Ukraine', 'United Kingdom', 'Vatican City'];
-
-  return ListView.builder(
-    itemCount: europeanCountries.length,
-    itemBuilder: (context, index) {
-      return ListTile(
-        title: Text(europeanCountries[index]),
-      );
-    },
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.wb_sunny),
+        title: Text('Sun'),
+      ),
+      ListTile(
+        leading: Icon(Icons.brightness_3),
+        title: Text('Moon'),
+      ),
+      ListTile(
+        leading: Icon(Icons.star),
+        title: Text('Star'),
+      ),
+    ],
   );
 }
+
+
 
