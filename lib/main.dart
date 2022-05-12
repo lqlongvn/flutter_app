ec9f6c7fe88b818c9367186d74739708e39abfbf
lqlongvn <lqlongvn@gmail.com>
2022-05-12 21:36:06 +0700

    ListView-2

--- lib/main.dart
+++ lib/main.dart
@@ -28,5 +28,17 @@ class BodyLayout extends StatelessWidget {
 
 // replace this function with the code in the examples
 Widget _myListView(BuildContext context) {
-  return ListView();
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        title: Text('Dog'),
+      ),
+      ListTile(
+        title: Text('Cat'),
+      ),
+      ListTile(
+        title: Text('Rabbit'),
+      ),
+    ],
+  );
 }
