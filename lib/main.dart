a19b47e58c1cea8f02df3f53b95ca376a152e59a
lqlongvn <lqlongvn@gmail.com>
2022-05-15 16:12:59 +0700

    Touch Detection (2)

--- lib/main.dart
+++ lib/main.dart
@@ -28,37 +28,32 @@ class BodyLayout extends StatelessWidget {
 
 // replace this function with the code in the examples
 Widget _myListView(BuildContext context) {
-
-  // the Expanded widget lets the columns share the space
-  Widget column = Expanded(
-    child: Column(
-      // align the text to the left instead of centered
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text('Title', style: TextStyle(fontSize: 16),),
-        Text('subtitle'),
-      ],
-    ),
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        title: Text('Sun'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Sun');
+        },
+      ),
+      ListTile(
+        title: Text('Moon'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Moon');
+        },
+      ),
+      ListTile(
+        title: Text('Star'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Star');
+        },
+      ),
+    ],
   );
-
-  return ListView.builder(
-    itemBuilder: (context, index) {
-      return Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: <Widget>[
-              column,
-              column,
-            ],
-          ),
-        ),
-      );
-    },
-  );
-
 }
-
 
 
 
