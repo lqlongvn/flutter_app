015d77799bf418e6c828f5be3da6984d14a3f271
lqlongvn <lqlongvn@gmail.com>
2022-05-15 16:25:21 +0700

    Updating list data ListView

--- lib/main.dart
+++ lib/main.dart
@@ -19,43 +19,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class BodyLayout extends StatelessWidget {
+class BodyLayout extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return _myListView(context);
+  BodyLayoutState createState() {
+    return new BodyLayoutState();
   }
 }
 
-// replace this function with the code in the examples
-Widget _myListView(BuildContext context) {
-  return ListView(
-    children: <Widget>[
-      ListTile(
-        title: Text('Sun'),
-        trailing: Icon(Icons.keyboard_arrow_right),
-        onTap: () {
-          print('Sun');
-        },
-      ),
-      ListTile(
-        title: Text('Moon'),
-        trailing: Icon(Icons.keyboard_arrow_right),
-        onTap: () {
-          print('Moon');
-        },
-      ),
-      ListTile(
-        title: Text('Star'),
-        trailing: Icon(Icons.keyboard_arrow_right),
-        onTap: () {
-          print('Star');
-        },
-      ),
-    ],
-  );
+class BodyLayoutState extends State<BodyLayout> {
+
+  List<String> titles = ['Sun', 'Moon', 'Star'];
+
+  @override
+  Widget build(BuildContext context) {
+    return _myListView();
+  }
+
+  Widget _myListView() {
+    return ListView.builder(
+      itemCount: titles.length,
+      itemBuilder: (context, index) {
+        final item = titles[index];
+        return Card(
+          child: ListTile(
+            title: Text(item),
+
+            onTap: () { //                                  <-- onTap
+              setState(() {
+                titles.insert(index, 'Planet');
+              });
+            },
+
+            onLongPress: () { //                            <-- onLongPress
+              setState(() {
+                titles.removeAt(index);
+              });
+            },
+
+          ),
+        );
+      },
+    );
+  }
 }
-
-
 
 
 
