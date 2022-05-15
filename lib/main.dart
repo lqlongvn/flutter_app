239bc5958b642e29e1386e36e834a7c0d0b313ce
lqlongvn <lqlongvn@gmail.com>
2022-05-15 16:02:55 +0700

    Touch Detection

--- lib/main.dart
+++ lib/main.dart
@@ -28,27 +28,33 @@ class BodyLayout extends StatelessWidget {
 
 // replace this function with the code in the examples
 Widget _myListView(BuildContext context) {
-
-  final titles = ['bike', 'boat', 'bus', 'car',
-    'railway', 'run', 'subway', 'transit', 'walk'];
-
-  final icons = [Icons.directions_bike, Icons.directions_boat,
-    Icons.directions_bus, Icons.directions_car, Icons.directions_railway,
-    Icons.directions_run, Icons.directions_subway, Icons.directions_transit,
-    Icons.directions_walk];
-
-  return ListView.builder(
-    itemCount: titles.length,
-    itemBuilder: (context, index) {
-      return Card( //                           <-- Card widget
-        child: ListTile(
-          leading: Icon(icons[index]),
-          title: Text(titles[index]),
-        ),
-      );
-    },
+  return ListView(
+    children: <Widget>[
+      ListTile(
+        title: Text('Sun'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Sun');
+        },
+      ),
+      ListTile(
+        title: Text('Moon'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Moon');
+        },
+      ),
+      ListTile(
+        title: Text('Star'),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {
+          print('Star');
+        },
+      ),
+    ],
   );
 }
+
 
 
 
